480e54eb8f8d1442d9d22c49d786bd9a3c5df970
mahreen14 <112086377+mahreen14@users.noreply.github.com>
2023-04-06 10:59:03 +0530

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <style>
+    body {
+background-image: url('https://www.canva.com/design/DAFfTACU7gw/jprtPHVeppbtCa7BtrofEQ/edit?analyticsCorrelationId=598a546d-fb5d-4905-9b47-0bce91642838');
+background-repeat: no-repeat;
+background-size: 1080px;
+    } </style> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main restro</title>
+    <link rel="stylesheet" href="mainpage.css"> 
+</head>
+<body>
+    <div class="mainr" > 
+      
+        <h1 class="title">RESTAURANTS</h1>
+                <div class="restro-content">
+                    <h2> R<span>estaurants</span> </h2>
+                 <a href="restro1.php"> 
+                   <img src="https://sarathcitycapitalmall.com/wp-content/uploads/2020/03/Pista-house-1250x250-1.jpg" alt="AVATAR" class="avatar"> </a>
+               
+                 <a href="restro2.php"> 
+                    <img src="https://yt3.ggpht.com/a/AATXAJwQIppKyjy0gZMtGDW2dtku76EFsg_NidyLSg=s900-c-k-c0xffffffff-no-rj-mo" alt="AVATAR" class="avatar"> </a>
+                </div>
+                <div class="cafe-content">
+                    <h2> C<span>afes</span> </h2>
+                 <a href="cafe1.php"> 
+                   <img src="https://tse2.mm.bing.net/th?id=OIP.z1sTlaN_A3sGIz9CxxQh0gAAAA&pid=Api&P=0" alt="AVATAR" class="avatar"> </a>
+               
+                 <a href="cafe2.php"> 
+                    <img src="https://thumbs.dreamstime.com/b/coffee-cup-vector-icon-smoke-illustration-171102772.jpg" alt="AVATAR" class="avatar"> </a>
+                </div> 
+        </div>
+</body> 
+</html>
